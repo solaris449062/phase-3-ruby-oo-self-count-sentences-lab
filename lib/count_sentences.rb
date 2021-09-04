95ffe5bc817887752d259c9baea4793ff8a11016
Sungoh Park <spark449062@gmail.com>
2021-09-04 17:41:49 -0600

    commit tree 0b20b92b278b075304b1278df71a95843b50d53a

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") ? true : false
   end
 
   def question?
-
+    self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+    self.end_with?("!") ? true : false
   end
 
   def count_sentences
-
-  end
+    split_string = self.split(/[.!?]\s/)
+    split_string.length
+    end
 end
